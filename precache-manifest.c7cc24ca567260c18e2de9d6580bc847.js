540e77eeabe288c4586e6c681b52917a263b3d8d
razorsrider <razorsrider@gmail.com>
2020-05-29 15:56:33 +0400

    Updates

---
+++ precache-manifest.c7cc24ca567260c18e2de9d6580bc847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe11dfeb59305f0cff1c5771a65cb34",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "b6e1159551c403f20cfc",
+    "url": "/react-redux/static/js/2.24100ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux/static/js/2.24100ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fb9489ee4cf98ab193",
+    "url": "/react-redux/static/js/main.6d01713e.chunk.js"
+  },
+  {
+    "revision": "603a77d2b5d8cfe4a017",
+    "url": "/react-redux/static/js/runtime-main.4326449f.js"
+  }
+]);
